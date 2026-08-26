1b5b70ae26eb8066633fe316a83c60815a0abc43
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 08:40:41 +0100

    Add age calculation program in C

---
+++ Algo_et_programmation_1/Langage_C/01_calcul_age.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int X,S;
+	printf("Entrer l'annee de naissace:\n");
+	scanf("%d",&X);
+	S=2025-X;
+	printf("votre age est: %d ",S);
+	return 0;
+}
