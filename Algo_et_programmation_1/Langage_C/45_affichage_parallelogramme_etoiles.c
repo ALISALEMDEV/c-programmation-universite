749803ad222cccc01fbe67fc219bb7ab4094a344
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 09:47:33 +0100

    Add program to display parallelogram of asterisks
    This program prompts the user for a number and displays a parallelogram of asterisks based on the input.

---
+++ Algo_et_programmation_1/Langage_C/45_affichage_parallelogramme_etoiles.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int L,k,i,j;
+	printf("entrer le nombre L :\n");
+	scanf("%d",&L);
+	for(i=1;i<=L;i++){
+		for(j=1;j<=L-i;j++){
+		printf(" ");
+		}
+	    for(j=1;j<=L;j++){
+
+			printf("*");
+	}
+		printf("\n");	
+	}
+	return 0;
+}
