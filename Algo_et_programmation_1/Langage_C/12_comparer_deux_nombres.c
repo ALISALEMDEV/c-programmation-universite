465a38c6994edb076b6fba038f485798d36a5df1
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 08:55:30 +0100

    Add program to compare two numbers in C

---
+++ Algo_et_programmation_1/Langage_C/12_comparer_deux_nombres.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int A,B;
+	printf("entrer les deux nombres A et B:\n");
+	scanf("%d%d",&A,&B);
+	if(A==B)
+	printf("les deux nombres sont egaux\n");
+	else
+	printf("les deux nombres ne sont pas egaux");
+	return 0;
+}
