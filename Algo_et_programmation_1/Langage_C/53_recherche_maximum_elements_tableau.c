d4f48a86546c67202b51f1356939ce0b98a81faa
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 12:52:32 +0100

    Add C program to find maximum in an array
    Implement a C program to find the maximum value in an array of 10 integers.

---
+++ Algo_et_programmation_1/Langage_C/53_recherche_maximum_elements_tableau.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i,T[10],temp;
+	
+	for(i=0;i<10;i++){
+		printf("donner la valeur de T[%d]:",i);
+		scanf("%d",&T[i]);
+		if(i==0)
+		temp=T[i];
+		if(temp<T[i])
+		temp=T[i];
+	}
+	printf("le maximum est : %d",temp);
+	return 0;
+}
